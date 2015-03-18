a21f7df40e07ee0d047efa3cdaa84b9fdef460f9
windowsazure <windowsazure>
2015-03-18 07:53:55 +0000

    Committing update from request /api/scmvfs/service/api/overdue.js

--- service/api/overdue.js
+++ service/api/overdue.js
@@ -5,7 +5,3 @@ exports.post = function(request, response) {
 
     response.send(statusCodes.OK, { message : 'Hello World!' });
 };
-
-exports.get = function(request, response) {
-    response.send(statusCodes.OK, { message : 'Hello World!' });
-};
