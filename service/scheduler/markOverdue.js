a87a188bd397b43027a24fe67de44652d31cf9ac
windowsazure <windowsazure>
2015-03-18 13:28:28 +0000

    Committing update from request /api/scmvfs/service/scheduler/markOverdue.js

--- service/scheduler/markOverdue.js
+++ service/scheduler/markOverdue.js
@@ -1,15 +1,20 @@
 function markOverdue() {
     var today = new Date();
     var todoItems = tables.getTable('TodoItem');
+    console.log('job started: '+today);
     todoItems.read({
         success: markOverdue
     });
         
     function markOverdue(results) {
+        console.log('# of results: '+results.length);
         results.forEach(function(item) {
-            if(item.dueDate > today) {
+            console.log('checking item: '+item);
+            if(item.duedate > today) {
+                console.log('item duedate: '+item.duedate);
                 item.overdue = true;
                 todoItems.update(item);
+                console.log('item marked!');
             }
         });
     }
