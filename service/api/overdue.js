783c4356e4fda29734783b6d6802d359a0ff5fbf
windowsazure <windowsazure>
2015-03-18 08:14:05 +0000

    Committing update from request /api/scmvfs/service/api/overdue.js

--- service/api/overdue.js
+++ service/api/overdue.js
@@ -2,5 +2,14 @@ exports.post = function(request, response) {
     var tables = request.service.tables;
     var push = request.service.push;
     var todoItems = tables.getTable('TodoItem');
+    todoItems.read( {
+        success: markOverdue
+        });
+        
+    function markOverdue(results) {
+        results.forEach(function(item) {
+            if(item.)
+        });
+    }
     response.send(statusCodes.OK, { message : 'Hello World!' });
 };
