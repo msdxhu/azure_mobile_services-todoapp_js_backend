becafd04e62661f498dea2e6a9a5e279f055749c
windowsazure <windowsazure>
2015-03-18 08:29:54 +0000

    Committing update from request /api/scmvfs/service/api/completeall.js

--- service/api/completeall.js
+++ service/api/completeall.js
@@ -1,11 +1,23 @@
 exports.post = function(request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
+    var tables = request.service.tables;
     //   var push = request.service.push;
-
-    response.send(statusCodes.OK, { message : 'Hello World!' });
-};
-
-exports.get = function(request, response) {
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    var user = request.user;
+    var todoItem = tables.table('TodoItem');
+    
+    todoItem.where({
+        userId: user.userId, 
+        complete: false
+    }).read({
+        success: completeAll
+    });
+    
+    function completeAll(items){
+        items.forEach(function(item){
+            item.complete = true;
+            todoItem.update(item);
+        });
+    }
+    
+    response.send(statusCodes.OK);
 };
